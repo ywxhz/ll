0d7c1abc1e46225e18861a16e0bbcffe85acd3a1
ywxhz <yw@csxhz.cn>
2018-04-19 11:46:28 +0800

    asd

--- lvyou.py
+++ lvyou.py
@@ -1,19 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_bootstrap import Bootstrap
+from DataService import DataServicec
 
 app = Flask(__name__)
 Bootstrap(app)
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+db = DataServicec()
 
 @app.route('/seach')
 def hello_world():
     return render_template('seach.html')
 
+<<<<<<< Updated upstream
 @app.route('/test')
 def hello_world1():
     return render_template('test.html')
 
 @app.route('/test')
+=======
+@app.route('/create-travel',methods=['POST'])
+def create_travel():
+    if request.method == 'POST':
+        id = db.NewProject(request.form)
+    return jsonify({'id': id})
+
+@app.route('/user/<name>')
+>>>>>>> Stashed changes
 def user(name):
     return render_template('test.html', name=name)
 
